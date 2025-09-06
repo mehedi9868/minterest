23bd26fcf44f68aaf4bcc18274c1da14c831c31d
Mehedi Hasan <36736490+mehedi9868@users.noreply.github.com>
2025-09-06 19:52:58 +0600

    Update app_min.js

--- app_min.js
+++ app_min.js
@@ -2,7 +2,8 @@
 
 // ===== Config (set these) =====
 const API_KEY = "AIzaSyDUE_P391KSJ4gC6-FG8ch-XeinTh4gUT8";         // <-- আপনার Drive API key
-const REQUIRED_PASSWORD = "iloveyou" || "50607080";                             // <-- পাসওয়ার্ড
+const REQUIRED_PASSWORD = "iloveyou";                             // <-- পাসওয়ার্ড
+const ANOTHER_PASSWORD = 50607080;
 // ডিফল্ট ড্রাইভ ফোল্ডার (যদি ইউজার লিংক না দেয় বা ভুল দেয়, এটা ব্যবহার হবে)
 const DEFAULT_DRIVE_URL = "https://drive.google.com/drive/folders/12qLQqg_gjw7gGcmbJ4dIGeNe6iRigahy"; // <-- এখানে আপনার ডিফল্ট ফোল্ডার লিংক বসান
 // =================================
@@ -173,7 +174,7 @@ async function handleAdd(){
   try{
     const rawLink = (linkInput?.value || '').trim();
     const pwd = (pwdInput?.value || '').trim().toLowerCase();
-    if(pwd !== REQUIRED_PASSWORD){
+    if(pwd !== REQUIRED_PASSWORD || ANOTHER_PASSWORD){
       showToast('ভুল পাসওয়ার্ড ⚠️');
       return;
     }
